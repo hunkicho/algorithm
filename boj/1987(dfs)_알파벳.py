90021910f5b76a18451dc5a109a2bfc3e4fc2066
hunkicho <jgchk4814@gmail.com>
2023-05-16 00:21:48 +0900

    :clown_face: Feat: Solve with solution
    - First i use BFS but time over.
    - So i search solution and problem is duplication route(test case3 -> IEHFA)
    - Many users of python use set data structure to avoid duplication
    - I never think about them
    - I should stop thinking only of the standardized way

--- boj/1987(dfs)_알파벳.py
+++ boj/1987(dfs)_알파벳.py
@@ -1,38 +1,34 @@
 import sys
 
-
-def dfs(x, y, cnt):
+def bfs(x, y):
     global result
+    q = set()
+    q.add((x, y, board[x][y]))
 
-    if max(result) > cnt + (R * C - cnt):
-        return
+    while q:
+        xx, yy, history = q.pop()
+        result = max(result, len(history))
 
-    result.add(cnt)
+        for i in range(4):
+            nx = xx + dx[i]
+            ny = yy + dy[i]
 
-    for i in range(4):
-        qx = x + dx[i]
-        qy = y + dy[i]
-
-        if qx < 0 or qx >= R or qy < 0 or qy >= C:
-            continue
-        else:
-            if history[ord(board[qx][qy])] == 0:
-                history[ord(board[qx][qy])] = 1
-                dfs(qx, qy, cnt + 1)
-                history[ord(board[qx][qy])] = 0
+            if nx < 0 or nx >= R or ny < 0 or ny >= C:
+                continue
+            else:
+                if board[nx][ny] not in history:
+                    q.add((nx, ny, history + board[nx][ny]))
 
 
 if __name__ == "__main__":
     dx = [0, 0, -1, 1]
     dy = [1, -1, 0, 0]
     board = []
-    history = [0 for _ in range(ord('Z') + 1)]
-    result = set([1])
+    result = 1
 
     R, C = map(int, sys.stdin.readline().split())
     for _ in range(R):
         board.append(list(sys.stdin.readline().rstrip()))
 
-    history[ord(board[0][0])] = 1
-    dfs(0, 0, 1)
-    print(max(result))
+    bfs(0, 0)
+    print(result)
