79a8b1f46feab86b2854236de605dc49795b9b6e
hunkicho <jgchk4814@gmail.com>
2023-08-07 17:52:41 +0900

    :white_check_mark: Practice test
    - Practice test
    - Try again after test

---
+++ programmers/짝지어 제거하기.py
@@ -0,0 +1,10 @@
+def solution(s):
+    stack = []
+    for case in s:
+        if stack and stack[-1] == case:
+            stack.pop()
+        else:
+            stack.append(case)
+    return 0 if stack else 1
+
+print(solution("baabaa"))
