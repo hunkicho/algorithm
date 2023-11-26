95927b9eec3ae7ccf6cb2646e84ee2fffaf17314
hunkicho <jgchk4814@gmail.com>
2023-11-26 13:53:36 +0900

    :sparkles: Feat: Solve with solution
    - Solve with solution :(

---
+++ boj/17952(stack)_과제는 끝나지 않아.py
@@ -0,0 +1,24 @@
+# stack
+
+import sys
+
+n = int(sys.stdin.readline())
+stack = []
+score = 0
+
+for _ in range(n):
+    info = list(map(int, sys.stdin.readline().split()))
+
+    if info[0] > 0:
+        if info[2] == 1:
+            score += info[1]
+        else:
+            stack.append([info[1], info[2]-1])
+    else:
+        if stack:
+            stack[-1][1] = stack[-1][1] - 1
+            if stack[-1][1] == 0:
+                #score += stack[-1][0]
+                work = stack.pop()
+                score += work[0]
+print(score)
