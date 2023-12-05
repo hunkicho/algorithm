4f450122423a35a7771e067631e525749c5362bf
hunkicho <jgchk4814@gmail.com>
2023-12-05 09:20:35 +0900

    :recycle: Feat: refactor code
    - use hash

--- leetcode/two-sum.py
+++ leetcode/two-sum.py
@@ -7,3 +7,13 @@ class Solution:
                 if nums[i] + nums[j] == target:
                     result = [i, j]
                     return result
+                
+# use hash
+class Solution:
+    def twoSum(self, nums: list[int], target: int) -> list[int]:
+        hashmap = {}
+        for i in range(len(nums)):
+            complement = target - nums[i]
+            if complement in hashmap:
+                return [i, hashmap[complement]]
+            hashmap[nums[i]] = i
